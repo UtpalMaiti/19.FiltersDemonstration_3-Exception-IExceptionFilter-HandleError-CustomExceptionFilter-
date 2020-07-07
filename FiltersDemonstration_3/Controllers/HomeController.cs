bb5e78715933b3ed97322a2cb0885e3771333387
Utpal Maiti <Utpalmaiti12@gmail.com>
2020-07-07 08:14:51 +0530

    Add project files.

---
+++ FiltersDemonstration_3/Controllers/HomeController.cs
@@ -0,0 +1,28 @@
+﻿using FiltersDemonstration_3.UtilityClasses;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FiltersDemonstration_3.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        [HandleError(View = "TestError", ExceptionType = typeof(ArgumentOutOfRangeException))]
+        [HandleError(View = "Error")]
+        //[CustomExceptionFilter]
+        public ActionResult Index(int? id)
+        {
+            if (id.HasValue)
+            {
+                throw new ArgumentOutOfRangeException();
+            }
+
+            // throw new IndexOutOfRangeException();
+
+            return View();
+        }
+    }
+}
---
+++ FiltersDemonstration_3/UtilityClasses/CustomExceptionFilterAttribute.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FiltersDemonstration_3.UtilityClasses
+{
+    public class CustomExceptionFilterAttribute : FilterAttribute, IExceptionFilter
+    {
+        public void OnException(ExceptionContext filterContext)
+        {
+            string controllerName = filterContext.RouteData.Values["controller"].ToString();
+
+            string actionName = filterContext.RouteData.Values["action"].ToString();
+
+            Exception ex = filterContext.Exception;
+
+            //Write the Code to Log this Exception...
+
+            filterContext.Result = new ViewResult
+            {
+                ViewName = "CustomErrorView"
+            };
+
+            filterContext.ExceptionHandled = true;
+        }
+    }
+}
